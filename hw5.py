c542bc56108710c0880860d31b12f449d3223f48
amellola <79540827+amellola@users.noreply.github.com>
2022-01-07 12:57:55 +0100

    Add files via upload

---
+++ hw5.py
@@ -0,0 +1,15 @@
+class Rectangle:
+    
+    def __init__(self,length,width):
+        self.length=length
+        self.width=width
+        def Area(length,width):
+            return(length*width)
+class Vehicule:
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+class Vehicule1:
+    pass
+class Bus(Vehicule):
+    pass
